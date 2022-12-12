edfb20aeaba3ff94a3d97dad951003a7a139c801
boldoed <70879615+boldoed@users.noreply.github.com>
2022-12-12 19:36:27 +0700

    Add files via upload

---
+++ T3_3_recsum.pas
@@ -0,0 +1,16 @@
+﻿program rec_sum;
+
+function R_Sum(n : integer) : integer;
+  begin
+    if n = 0 then
+      R_Sum := 0
+    else 
+      R_Sum := (n mod 10) + R_Sum(n div 10);
+  end;
+  
+begin
+  var n : integer;
+  writeln('Для какого числа вы хотите посчитать сумму цифр? Введите его.');
+  readln(n);
+  writeln(R_Sum(n))
+end.
