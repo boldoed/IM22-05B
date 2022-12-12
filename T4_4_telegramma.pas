9f0b0394128a504d2d9aa06e90c34f1b24541e25
boldoed <70879615+boldoed@users.noreply.github.com>
2022-12-12 20:05:25 +0700

    Add files via upload

---
+++ T4_4_telegramma.pas
@@ -0,0 +1,16 @@
+﻿program telegramma;
+
+function Translate(stroka : string) : string;
+  begin
+    stroka := stroka.Replace(' ЗПТ', ',');
+    stroka := stroka.Replace(' ТЧК', '.');
+    result := stroka
+  end;
+  
+var 
+  stroka : string; 
+begin
+  writeln('Введите текст для перевода');
+  readln(stroka); 
+  writeln(Translate(stroka));
+end.
