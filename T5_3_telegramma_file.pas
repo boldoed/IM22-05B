d151e8fc00567d08c02dcd19d46c8a7742645230
boldoed <70879615+boldoed@users.noreply.github.com>
2022-12-13 23:31:34 +0700

    Add files via upload

---
+++ T5_3_telegramma_file.pas
@@ -0,0 +1,28 @@
+﻿program telegramma_file;
+
+var
+  s:string;
+  f:TextFile;
+  
+function Translate(stroka : string) : string;
+  begin
+    stroka := stroka.Replace(' ЗПТ', ',');
+    stroka := stroka.Replace(' ТЧК', '.');
+    stroka := stroka.Replace(' ДВТ', ':');
+    stroka := stroka.Replace(' ВСК', '!');
+    stroka := stroka.Replace(' КВЧ', '"');
+    stroka := stroka.Replace('КВЧ ', '"');
+    stroka := stroka.Replace(' ДФС', ' -');
+    result := stroka  
+  end;
+  
+begin
+  AssignFile(f,'D:\code_file\test_file.txt');
+  Reset(f, Encoding.UTF8);
+  while not Eof(f) do
+  begin
+    readln(f,s);
+    writeln(Translate(s));
+  end;
+  CloseFile(f);
+end.
