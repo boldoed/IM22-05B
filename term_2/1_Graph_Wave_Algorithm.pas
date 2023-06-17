ab80e83ba4401f2bd02239783990f5de1a4522cb
boldoed <70879615+boldoed@users.noreply.github.com>
2023-06-17 17:49:36 +0700

    Add files via upload

--- term_2/1_Graph_Wave_Algorithm.pas
+++ term_2/1_Graph_Wave_Algorithm.pas
@@ -1,30 +1,27 @@
-﻿program Wave_algorithm;
+﻿program Wave_Algorithm;
 
 const
   COUNT_VERT = 5;
-  
+
 type
   TMatrix = array[1..COUNT_VERT, 1..COUNT_VERT] of integer;
 
 var
   Graph: TMatrix;
-  
-procedure BuildMatrix(var Graph: TMatrix);
+
+procedure InitGraph(var Graph: TMatrix);
+var
+  i, j: integer;
 begin
-  for var i := 1 to COUNT_VERT do
-    for var j := 1 to COUNT_VERT do
+  for i := 1 to COUNT_VERT do
+    for j := 1 to COUNT_VERT do
       Graph[i, j] := 0;
 end;
 
-procedure RandomFill(var Graph: TMatrix);
+procedure AddEdge(var Graph: TMatrix; vert1, vert2: integer);
 begin
-  for var i := 1 to COUNT_VERT do
-    for var j := i+1 to COUNT_VERT do
-      if Random(2) = 1 then
-        begin
-          Graph[i, j] := 1;
-          Graph[j, i] := 1;
-        end;
+  Graph[vert1, vert2] := 1;
+  Graph[vert2, vert1] := 1;
 end;
 
 procedure PrintMatrix(Graph: TMatrix);
@@ -42,48 +39,96 @@ begin
   end;
 end;
 
-procedure WaveAlgorithm(Graph: TMatrix; startVert: integer);
+procedure WaveAlgorithm(Graph: TMatrix; startVert, finishVert: integer);
 var
-  Queue: array[1..COUNT_VERT] of integer;
   Visit: array[1..COUNT_VERT] of integer;
+  Queue: array[1..COUNT_VERT] of integer;
   Dist: array[1..COUNT_VERT] of integer;
-  i, front, back, СhoseVert, SosedVert: integer;
+  Path: array[1..COUNT_VERT] of integer;
+  front, next, currVert, sosedVert: integer;
+  pathFound: boolean;
+  pathLength: integer;
 begin
-  for i := 1 to COUNT_VERT do
+  // Инициализация
+  for currVert := 1 to COUNT_VERT do
   begin
-    Queue[i] := 0;
-    Visit[i] := 0;
-    Dist[i] := 0;
+    Visit[currVert] := 0;
+    Dist[currVert] := 0;
   end;
 
-  Queue[1] := startVert;
-  Visit[startVert] := 1;
-  back := 1;
-  
+  // Начальная вершина
   front := 1;
-  while front <= back do
+  next := 1;
+  Queue[next] := startVert;
+  Visit[startVert] := 1;
+
+  // Волновой алгоритм
+  pathFound := False;
+  while (front <= next) and (not pathFound) do
   begin
-    СhoseVert := Queue[front];
-    for SosedVert := 1 to COUNT_VERT do
+    currVert := Queue[front];
+    for sosedVert := 1 to COUNT_VERT do
     begin
-      if Graph[СhoseVert, SosedVert] and (not Visit[SosedVert]) = 1 then
+      if (Graph[currVert, sosedVert] = 1) and (Visit[sosedVert] = 0) then
       begin
-        back += 1;
-        Queue[back] := SosedVert;
-        Visit[SosedVert] := 1;
-        Dist[SosedVert] := Dist[СhoseVert] + 1;
+        next += 1;
+        Queue[next] := sosedVert;
+        Visit[sosedVert] := 1;
+        Dist[sosedVert] := Dist[currVert] + 1;
+        if sosedVert = finishVert then
+        begin
+          pathFound := True;
+          Break;
+        end;
       end;
     end;
     front += 1;
   end;
-  
-  for i := 1 to COUNT_VERT do
-    Writeln('Расстояние от вершины ', startVert, ' до вершины ', i, ' -- ', Dist[i]);
+
+  // Восстановление пути
+  if pathFound then
+  begin
+    pathLength := Dist[finishVert] + 1;
+    Path[pathLength] := finishVert;
+    currVert := finishVert;
+    for var i := pathLength - 1 downto 1 do
+    begin
+      for sosedVert := 1 to COUNT_VERT do
+      begin
+        if (Graph[currVert, sosedVert] = 1) and (Dist[sosedVert] = i - 1) then
+        begin
+          Path[i] := sosedVert;
+          currVert := sosedVert;
+          Break;
+        end;
+      end;
+    end;
+
+    // Вывод пути
+    Write('Путь: ');
+    for var i := 1 to pathLength do
+    begin
+      Write(Path[i]);
+      if i < pathLength then
+        Write(' -> ');
+    end;
+    WriteLn;
+    WriteLn('Расстояние: ', Dist[finishVert]);    
+  end
+  else
+  begin
+    WriteLn('Путь не найден');
+  end;
 end;
 
 begin
-  BuildMatrix(Graph);
-  RandomFill(Graph);
+  InitGraph(Graph);
+  AddEdge(Graph, 1, 2);
+  AddEdge(Graph, 1, 3);
+  AddEdge(Graph, 2, 4);
+  AddEdge(Graph, 3, 4);
+  AddEdge(Graph, 4, 5);
+
   PrintMatrix(Graph);
-  WaveAlgorithm(Graph, 1);
+  WaveAlgorithm(Graph, 1, 5);
 end.
