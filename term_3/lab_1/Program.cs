c3ff746acd79d683555c4de17e33754b824f9c7f
Danil Popov <70879615+boldoed@users.noreply.github.com>
2023-10-19 13:12:08 +0700

    Add files via upload

---
+++ term_3/lab_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace TelegrammaTCHK
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var telegramma = new Telegramma();
+            while (true)
+            {
+                Console.Write("Введите текст: ");
+                string text = Console.ReadLine();
+                if (text.Length != 0)
+                {
+                    Console.WriteLine(telegramma.TelegrammaWrite(text));
+                }
+                else
+                {
+                    Console.WriteLine("Вы ничего не ввели.");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ term_3/lab_1/TelegrammaTCHK.cs
@@ -0,0 +1,25 @@
+﻿namespace TelegrammaTCHK
+{
+    public class Telegramma
+    {
+
+        public string TelegrammaWrite(string teleStr) 
+        {
+            teleStr = telegrammaAnswer(teleStr);
+            return teleStr;
+        }
+        private string telegrammaAnswer(string teleStr)
+        {
+            string answer = "";
+            answer = teleStr.Replace(" ЗПТ", ",");
+            answer = answer.Replace(" ТЧК", ".");
+            answer = answer.Replace(" ДВТ", ":");
+            answer = answer.Replace(" ВСК", "!");
+            answer = answer.Replace(" КВЧ", "'");
+            answer = answer.Replace("КВЧ ", "'");
+            answer = answer.Replace(" ДФС", " -");
+            return answer;
+
+        }
+    }
+}
