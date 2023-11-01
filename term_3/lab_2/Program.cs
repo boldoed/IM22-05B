bb12d4199a79dd030e8bc2cf24a95298a677e3d1
Danil Popov <70879615+boldoed@users.noreply.github.com>
2023-11-01 23:57:35 +0700

    Add files via upload

---
+++ term_3/lab_2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Circle_Lab2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            const float PI = 3.14F;
+            var circle1 = new TCircle(0, 0, 10, PI);
+            //Console.WriteLine(circle1);
+            Console.WriteLine(circle1.GetInfo());
+        }
+    }
+}
