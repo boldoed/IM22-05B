c368a2d0eec4104d587b8175d0d3e0ee586f2f3b
Danil Popov <70879615+boldoed@users.noreply.github.com>
2023-12-20 23:58:13 +0700

    Add files via upload

---
+++ term_3/lab_3.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Circle_Lab3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            const float PI = 3.14F;
+            var circle1 = new TCircle(0, 0, 10, PI);
+            var apprcircle1 = new TApprCircle(0, 0, 10, PI, 6);
+            Console.WriteLine(circle1.GetInfo());
+            Console.WriteLine(apprcircle1.GetInfoAppr());
+        }
+    }
+}
---
+++ term_3/lab_3.1/TCircle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Circle_Lab3
+{
+    public class TCircle
+    {
+        private int Xcoord;
+        private int Ycoord;
+        private int radius;
+        private float PI;
+
+        public TCircle(int Xcoord, int Ycoord, int radius, float PI)
+        {
+            this.Xcoord = Xcoord;
+            this.Ycoord = Ycoord;
+            this.Radius = radius; 
+            this.PI = PI;
+        }
+
+        public int Radius
+        {
+            set
+            {
+                if (value <= 0)
+                    Console.WriteLine("Радиус не может быть меньше нуля");
+                else
+                    radius = value;
+            }
+            get { return radius; }
+        }
+
+        public float GetCircleLength()
+        {
+            return (2 * PI * radius);
+        }
+
+        public double GetCircleArea()
+        {
+            return PI *  Math.Pow(radius,2);
+        }
+
+        public string GetInfo()
+        {
+            string result = $"Информация об окружности:\n\tКоординаты центра: {this.Xcoord}, {this.Ycoord}" +
+                            $"\n\tРадиус: {this.radius}" +
+                            $"\n\tДлина окружности: {Math.Round(GetCircleLength(), 2)}" +
+                            $"\n\tПлощадь круга: {Math.Round(GetCircleArea(), 2)}";
+            return result;
+        }
+
+    }
+}
