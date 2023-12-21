c618b105bb647ae1db0d6cf7b56ae8dc653cae06
Danil Popov <70879615+boldoed@users.noreply.github.com>
2023-12-21 14:00:10 +0700

    Add files via upload

---
+++ term_3/lab_5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+
+namespace Circle_Lab4
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            TContainer container = new TContainer();
+            var path = "C:\\Users\\dania\\source\\repos\\LAB_4\\LAB_4\\text.txt";
+            container.LoadFromFile(path);
+            foreach (var elem in container.LoadList())
+            {
+                Console.WriteLine(elem.GetInfo());
+            }
+            var maximumrad = container.MaxRadius();
+            foreach (var elem in container.LoadList())
+            {
+                if (maximumrad == elem.Radius)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("---Окружность с наибольшим радиусом---");
+                    Console.WriteLine(elem.GetInfo());
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("---Контейнер упорядоченный по длине окружности---");
+            foreach (var elem in container.SortCont())
+            {
+                Console.WriteLine(elem.GetInfo());
+            }
+        }
+    }
+}
---
+++ term_3/lab_5/TCircle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Circle_Lab4
+{
+    public class TCircle
+    {
+        private int Xcoords;
+        private int Ycoord;
+        private int radius;
+        private float PI;
+
+        public TCircle(int Xcoord, int Ycoord, int radius, double PI)
+        {
+            this.Xcoords = Xcoord;
+            this.Ycoord = Ycoord;
+            this.Radius = radius;
+            this.PI = (float)PI;
+        }
+
+        public int Radius
+        {
+            set
+            {
+                if (value <= 0)
+                    Console.WriteLine("Радиус должен быть положительным");
+                else
+                    radius = value;
+            }
+            get { return radius; }
+        }
+
+        public float GetCircleLength()
+        {
+            return (2 * PI * radius);
+        }
+
+        public double GetCircleArea()
+        {
+            return PI * Math.Pow(radius, 2);
+        }
+
+        public virtual string GetInfo()
+        {
+            string result = $"Информация об окружности:\n\tКоординаты центра: {this.Xcoords}, {this.Ycoord}" +
+                            $"\n\tРадиус: {this.radius}" +
+                            $"\n\tДлина окружности: {Math.Round(GetCircleLength(), 2)}" +
+                            $"\n\tПлощадь круга: {Math.Round(GetCircleArea(), 2)}";
+            return result;
+        }
+
+    }
+}
